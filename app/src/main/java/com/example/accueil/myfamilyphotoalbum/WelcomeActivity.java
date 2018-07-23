d89d77acb6f9c668800894e5e8ac81482c1a94a0
accueil <marionfontaine904@gmail.com>
2018-07-24 01:06:49 +0200

    navigation and SplashScreen

--- app/src/main/java/com/example/accueil/myfamilyphotoalbum/WelcomeActivity.java
+++ app/src/main/java/com/example/accueil/myfamilyphotoalbum/WelcomeActivity.java
@@ -1,13 +1,36 @@
 package com.example.accueil.myfamilyphotoalbum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public Button signIn, signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        signIn = (Button) findViewById(R.id.signIn);
+        signUp = (Button) findViewById(R.id.signUp);
+
+        signIn.setOnClickListener(this);
+        signUp.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if( view == signIn){
+            startActivity(new Intent(WelcomeActivity.this,SignInActivity.class));
+            finish();
+        }else if(view == signUp){
+            startActivity(new Intent(WelcomeActivity.this,SignUpActivity.class));
+            finish();
+        }
+
     }
 }
